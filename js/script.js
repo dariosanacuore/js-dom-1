f6e5719fd59c228ccb9a9899d5190fc5fe59aac4
dariosanacuore <dariosanacuore2@gmail.com>
2025-11-05 15:12:35 +0100

    feat:Completato l'esercizio

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 const btnElem = document.querySelector(".btn");
 const imgElem = document.querySelector("img");
+
+
+btnElem.addEventListener("click", function () {
+    imgElem.src = "./img/yellow_lamp.png";
+})
