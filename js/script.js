d8de15db63025d2bfa4f8d99433f8c3d15222d8e
dariosanacuore <dariosanacuore2@gmail.com>
2025-11-05 15:34:10 +0100

    feat:Completato parte del bonus

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 const btnElem = document.querySelector(".btn");
 const imgElem = document.querySelector("img");
 
-
+let accesa = false;
 btnElem.addEventListener("click", function () {
-    imgElem.src = "./img/yellow_lamp.png";
+    /** 
+    const isAccesa = imgElem.src.includes("yellow_lamp.png");
+    if (isAccesa === true) {
+        btnElem.classList.remove(imgElem.src = "./img/white_lamp.png");
+        btnElem.innerText = "Accendi";
+    } else {
+        btnElem.classList.add(imgElem.src = "./img/yellow_lamp.png");
+        btnElem.innerText = "Spegni";
+    }
+        */
+    if (accesa === true) {
+        imgElem.src = "./img/white_lamp.png";
+        btnElem.innerText = "Accendi";
+        accesa = false;
+    } else {
+        imgElem.src = "./img/yellow_lamp.png";
+        btnElem.innerText = "Spegni";
+        accesa = true;
+
+    }
+
 })
